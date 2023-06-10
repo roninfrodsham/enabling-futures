3069e0395b2f6386b23f116e71b86ead2f023ed8
roninfrodsham <roninfrodsham@gmail.com>
2023-06-11 00:53:00 +0100

    add test

--- app/utils/upload.server.ts
+++ app/utils/upload.server.ts
@@ -11,7 +11,7 @@ const extensions = ["docx", "doc", "pdf"]
 
 export const handleUpload = async (request: Request, context: AppLoadContext) => {
   const encoded = btoa(`${context.MAILJET_API_KEY}:${context.MAILJET_SECRET_KEY}`)
-
+  console.log(encoded);
   const formData = await unstable_parseMultipartFormData(request, unstable_createMemoryUploadHandler({
     maxPartSize: 1_000_000
   }))
