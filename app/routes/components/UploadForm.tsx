e4cda225712e39e7281689dae2ec1309cd44e6cd
roninfrodsham <roninfrodsham@gmail.com>
2023-06-11 10:12:19 +0100

    add state to show and hide form

--- app/routes/components/UploadForm.tsx
+++ app/routes/components/UploadForm.tsx
@@ -7,14 +7,16 @@ import Button from "./Button"
 import backgroundImage from '~/assets/background-cv.jpg'
 
 export default function UploadForm() {
-  if (typeof window !== 'undefined') {
-    const storage = localStorage.getItem("test")
-    if(storage == undefined) {
-      return null
-    }
-  }
+  // let showForm = false;
+  // if (typeof window !== 'undefined') {
+  //   const storage = localStorage.getItem("test")
+  //   if(storage !== undefined) {
+  //     showForm = true;
+  //   }
+  // }
   
   const actionData = useActionData()
+  const [showForm, setShowForm] = useState(false)
   const [allowPost, setAllowPost] = useState(true)
   const [errorMessage, setErrorMessage] = useState("")
 
@@ -40,48 +42,50 @@ export default function UploadForm() {
   }
 
   return (
-    <section className="relative overflow-hidden bg-blue-600 py-32">
-      <img
-        className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
-        src={backgroundImage}
-        alt=""
-        width={2347}
-        height={1244}
-      />
-      <Container className="relative">
-        <div className="mx-auto max-w-xl text-center">
-          <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
-            We would love to hear from you
-          </h2>
-          {
-            (actionData && actionData.success) ? (
-              <p className="mt-4 mb-8 text-lg tracking-tight text-white">
-                {actionData.success}
-              </p>
-            ) : (
-              <>
+    (showForm &&
+      <section className="relative overflow-hidden bg-blue-600 py-32">
+        <img
+          className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
+          src={backgroundImage}
+          alt=""
+          width={2347}
+          height={1244}
+        />
+        <Container className="relative">
+          <div className="mx-auto max-w-xl text-center">
+            <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl">
+              We would love to hear from you
+            </h2>
+            {
+              (actionData && actionData.success) ? (
                 <p className="mt-4 mb-8 text-lg tracking-tight text-white">
-                  Upload your CV directly to our HR department. We only accept Word Documents and PDF files. 
+                  {actionData.success}
                 </p>
-                <Form name="uploadcv" method="post" onSubmit={onSubmit} encType="multipart/form-data" className="text-white">
-                  { 
-                    actionData && actionData.error && (
-                      <p className=" mb-4 text-md font-semibold tracking-tight text-slate-900">{actionData.errorMessage}</p>
-                    )
-                  }
-                  {
-                      errorMessage !== "" && (
-                      <p className=" mb-4 text-md font-semibold tracking-tight text-slate-900">{errorMessage}</p>
-                    )
-                  }
-                  <input onChange={(e) => handleChange(e)} className="font-display text-sm cursor-pointer rounded-full border border-solid border-white bg-clip-padding mr-4 mb-4 px-3 py-[0.32rem] font-semibold leading-[2.15] text-white transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-white file:px-4 file:py-[0.32rem] file:text-slate-900 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary" id="cv" type="file" name="cv" />
-                  <Button color="white">Upload Document</Button>
-                </Form>
-              </>
-            )
-          }
-        </div>
-      </Container>
-    </section>
+              ) : (
+                <>
+                  <p className="mt-4 mb-8 text-lg tracking-tight text-white">
+                    Upload your CV directly to our HR department. We only accept Word Documents and PDF files. 
+                  </p>
+                  <Form name="uploadcv" method="post" onSubmit={onSubmit} encType="multipart/form-data" className="text-white">
+                    { 
+                      actionData && actionData.error && (
+                        <p className=" mb-4 text-md font-semibold tracking-tight text-slate-900">{actionData.errorMessage}</p>
+                      )
+                    }
+                    {
+                        errorMessage !== "" && (
+                        <p className=" mb-4 text-md font-semibold tracking-tight text-slate-900">{errorMessage}</p>
+                      )
+                    }
+                    <input onChange={(e) => handleChange(e)} className="font-display text-sm cursor-pointer rounded-full border border-solid border-white bg-clip-padding mr-4 mb-4 px-3 py-[0.32rem] font-semibold leading-[2.15] text-white transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:cursor-pointer file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-white file:px-4 file:py-[0.32rem] file:text-slate-900 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary" id="cv" type="file" name="cv" />
+                    <Button color="white">Upload Document</Button>
+                  </Form>
+                </>
+              )
+            }
+          </div>
+        </Container>
+      </section>
+    )
   )
 }
